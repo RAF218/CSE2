50846e5dddefbf8080fc46e886220ec8b63ccc83
raf218 <raf218@lehigh.edu>
2014-09-02 00:53:42 +0000

    updated Welcome Message

---
+++ hw01/hw01.java
@@ -0,0 +1,22 @@
+//Ralph Fernandez
+//hw 01
+//Welcome Message Jav Program
+
+//  define a class
+public class hw01{
+    
+    public static void main(String [ ] args) {
+        //Welcome Message
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-R--A--F--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v ");
+        //Autobiographic Statement        
+        System.out.println("This summer I went on vacation to Miami with my family, where we swam in the ocean and relaxed on the beach"
+        );
+    }
+}
